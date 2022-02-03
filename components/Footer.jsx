3db6e445eea186a885bdab03d07350bf4530810c
Piotr Maciejewski <pm@piotrs-macbook-pro.home>
2022-02-03 07:44:53 +0100

    footer udpated

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <div className="grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600"> 
       <div className="space-y-4 text-xs text-gray-800">
-          <h5 classname="font-bold">About</h5>
+          <h5 className="font-bold">About</h5>
           <p>How Airbnb works</p>
           <p>Newsroom</p>
           <p>Investors</p>
@@ -12,7 +12,7 @@ const Footer = () => {
           <p>AirBnB Luxe</p>
       </div>
       <div className="space-y-4 text-xs text-gray-800">
-          <h5 classname="font-bold text-gray-800">Home</h5>
+          <h5 className="font-bold text-gray-800">Home</h5>
           <p>How Airbnb works</p>
           <p>Newsroom</p>
           <p>Investors</p>
@@ -20,7 +20,7 @@ const Footer = () => {
           <p>AirBnB Luxe</p>
       </div>
       <div className="space-y-4 text-xs text-gray-800">
-          <h5 classname="font-bold text-gray-800">Other Staff</h5>
+          <h5 className="font-bold text-gray-800">Other Staff</h5>
           <p>How Airbnb works</p>
           <p>Newsroom</p>
           <p>Investors</p>
@@ -28,7 +28,7 @@ const Footer = () => {
           <p>AirBnB Luxe</p>
       </div>
       <div className="space-y-4 text-xs text-gray-800">
-          <h5 classname="font-bold text-gray-800">Membership</h5>
+          <h5 className="font-bold text-gray-800">Membership</h5>
           <p>How Airbnb works</p>
           <p>Newsroom</p>
           <p>Investors</p>
@@ -36,7 +36,7 @@ const Footer = () => {
           <p>AirBnB Luxe</p>
       </div>
       <div className="space-y-4 text-xs text-gray-800">
-          <h5 classname="font-bold text-gray-800">Newsletter</h5>
+          <h5 className="font-bold text-gray-800">Newsletter</h5>
           <p>How Airbnb works</p>
           <p>Newsroom</p>
           <p>Investors</p>
